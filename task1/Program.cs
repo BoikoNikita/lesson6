42850332066760c807a897ee44fb63b4c2333412
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-05-11 18:08:36 +0300

    example204

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите количество вводимых чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+
+void InNum(int M)
+{
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int A(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count ++; 
+  }
+  return count;
+}
+
+InNum(M);
+Console.WriteLine($"Введено чисел больше нуля: {A(array)} ");
